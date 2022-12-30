d2ed659b704ca2fd9f89fdd0761e63cf1a1812f3
Josue Ruiz <62705383+Josue-Ruiz@users.noreply.github.com>
2022-12-29 21:15:30 -0600

    Finish the configuration in MainActivity.kt and add internat permision

--- app/src/main/java/com/josue/example/superherolist/MainActivity.kt
+++ app/src/main/java/com/josue/example/superherolist/MainActivity.kt
@@ -2,16 +2,37 @@ package com.josue.example.superherolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.josue.example.superherolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    val superheros = listOf(
-        SuperHero("Spidernman","Peter Parker","Peter Parker","https://superherol")
-    )
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initRecyclerView()
     }
 
-    fun initRecyclerView() {}
+    fun initRecyclerView() {
+        binding.rvSuperhero.layoutManager = LinearLayoutManager(this)
+        val adapater =  HeroAdapter(getSuperheros())
+        binding.rvSuperhero.adapter = adapater
+    }
+
+    fun getSuperheros(): MutableList<SuperHero>{
+        val superheros:MutableList<SuperHero> = ArrayList()
+        superheros.add(SuperHero("Spiderman", "Marvel", "Peter Parker", "https://cursokotlin.com/wp-content/uploads/2017/07/spiderman.jpg"))
+        superheros.add(SuperHero("Daredevil", "Marvel", "Matthew Michael Murdock", "https://cursokotlin.com/wp-content/uploads/2017/07/daredevil.jpg"))
+        superheros.add(SuperHero("Wolverine", "Marvel", "James Howlett", "https://cursokotlin.com/wp-content/uploads/2017/07/logan.jpeg"))
+        superheros.add(SuperHero("Batman", "DC", "Bruce Wayne", "https://cursokotlin.com/wp-content/uploads/2017/07/batman.jpg"))
+        superheros.add(SuperHero("Thor", "Marvel", "Thor Odinson", "https://cursokotlin.com/wp-content/uploads/2017/07/thor.jpg"))
+        superheros.add(SuperHero("Flash", "DC", "Jay Garrick", "https://cursokotlin.com/wp-content/uploads/2017/07/flash.png"))
+        superheros.add(SuperHero("Green Lantern", "DC", "Alan Scott", "https://cursokotlin.com/wp-content/uploads/2017/07/green-lantern.jpg"))
+        superheros.add(SuperHero("Wonder Woman", "DC", "Princess Diana", "https://cursokotlin.com/wp-content/uploads/2017/07/wonder_woman.jpg"))
+        return superheros
+    }
 }
